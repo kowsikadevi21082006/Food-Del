f279768c0b3422aec4b87464ba5fd22402a7d579
kowsikadevi21082006 <deva36rp@gamil.com>
2024-10-21 17:43:06 +0530

    done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order fro frontend
 const placeOrder = async (req,res) => {
 
-    const frontend_url = "http://localhost:5174"
+    const frontend_url = "https://food-del-0jez.onrender.com"
 
     try {
         const newOrder = new orderModel({
